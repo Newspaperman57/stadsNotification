15ca3e7610ba85bd0c9d75e3c862643e23d3f314
newspaperman57 <mathias@pihlerne.dk>
2019-01-07 20:14:51 +0100

    Added PHP

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+	$command = '/usr/bin/python3 /var/www/stads.mathiaspihl.com/main.py '.escapeshellarg($_POST['user']).' '.escapeshellarg($_POST['pass']).' 2>&1';
+	//$command = 'echo "test"';
+	//echo $command;
+	echo exec($command);
+} else {
+echo "Use by POSTing 'user' and 'pass' as form-data and wait a short 35 seconds for an answer.... #throttling"; 
+}
